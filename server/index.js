ee7f84cb48884c3abbe3a801b56dcbc4609afd2e
daixusheng <204933796@qq.com>
2022-09-28 21:25:06 +0800

    .

--- server/index.js
+++ server/index.js
@@ -2,27 +2,57 @@ const express = require("express");
 
 const app = express();
 
-const port = 5000;
-
 // 设置跨域访问
-app.all("*", function (req, res, next) {
-  // 设置允许跨域的域名，* 代表允许任意域跨域访问
-  res.header("Access-Control-Allow-Origin", req.headers.origin || "*");
-  // 跨域允许的 header 类型
-  res.header("Access-Control-Allow-Headers", "Content-Type");
-  // 跨域允许的请求方式
-  res.header("Access-Control-Allow-Methods", "*");
+// app.all("*", function (req, res, next) {
+//   // 设置允许跨域的域名，* 代表允许任意域跨域访问
+//   res.header("Access-Control-Allow-Origin", "*");
+//   // 跨域允许的 header 类型
+//   res.header("Access-Control-Allow-Headers", "Content-Type");
+//   // 跨域允许的请求方式
+//   res.header("Access-Control-Allow-Methods", "*");
   
-  res.header("Content-Type", "application/json;charset=utf-8");
-  next();
-});
+//   res.header("Content-Type", "application/json;charset=utf-8");
+//   next();
+// });
+
+// 解析请求体
+// app.use(express.text());
+// app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// app.use(express.raw());
 
 app.get("/", (req, res) => {
-  res.send("hello world");
-})
+  const query = req.query;
+  res.send(query);
+});
+
+// app.get("/:name", (req, res) => {
+//   const params = req.params;
+//   res.send(params);
+// });
+
+// app.get("/api/books", (req, res) => {
+//   const query = req.query;
+//   res.send(query);
+// });
+
+// app.get("/api/books/:bookId", (req, res) => {
+//   const params = req.params;
+//   res.send(params);
+// });
+
+app.post("/", (req, res) => {
+  const body = req.body;
+  res.send(body);
+
+  // const params = req.params;
+  // res.send(params);
+
+  // const query = req.query;
+  // res.send(query);
+});
 
 
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.listen(5000, () => {
+  console.log("Server is running on port 5000");
 })
--- view/main.js
+++ view/main.js
@@ -1,11 +1,39 @@
 const submit = document.querySelector("#submit");
 
+const baseURL = "http://localhost:5000";
+
+// 拼接查询参数（执行一次只能拼接一个参数）
+function addURLParam(url, name, value) {
+  url += (url.indexOf("?") == -1 ? "?" : "&");
+  url += encodeURIComponent(name) + "=" + encodeURIComponent(value);
+  return url;
+}
+
 submit.addEventListener("click", function(event) {
   event.preventDefault();
 
+  // GET 请求
+  // const xhr = new XMLHttpRequest();
+  // xhr.onreadystatechange = function () {
+  //   if (xhr.readyState == 4) {
+  //     if ((xhr.status >= 200 && xhr.status < 300) || xhr.status == 304) {
+  //       console.log(xhr.responseText);
+  //     } else {
+  //       alert("Request was unsuccessful: " + xhr.status);
+  //     }
+  //   }
+  // };
+  // // xhr.open(Method, URL, Async)
+  // // xhr.open("get", "http://localhost:5000", true);
+  // // xhr.open("get", "http://localhost:5000/api/books?subject=JavaScript", true);
+  // xhr.open("get", "http://localhost:5000/api/books/10", true);
+  // xhr.send(null);
+
+
+  // POST 请求
   const xhr = new XMLHttpRequest();
   xhr.onreadystatechange = function () {
-    // console.log(xhr.readyState)
+    // console.log("readstatechange", xhr.readyState);
     if (xhr.readyState == 4) {
       if ((xhr.status >= 200 && xhr.status < 300) || xhr.status == 304) {
         console.log(xhr.responseText);
@@ -14,7 +42,38 @@ submit.addEventListener("click", function(event) {
       }
     }
   };
-  // xhr.open(Method, URL, Async)
-  xhr.open("get", "http://localhost:5000", true);
-  xhr.send(null);
+  // console.log("readstatechange", xhr.readyState);
+  xhr.open("post", "http://localhost:5000/", true);
+
+  // text/plain（纯文本类型）-> express.text()
+  // 当发送请求，请求体包含内容且未指定 Content-Type 类型时，浏览器会将其默认设置为 text/plain
+  // xhr.send("some content");
+
+  // application/json 类型
+  // 未设置 Content-type 时，默认为 text/plain -> express.text()
+  // 设置 Content-Type 为 application/json 时，-> express.json()
+  // const data = {name: "daixusheng", age: "20"};
+  // xhr.setRequestHeader("Content-Type", "application/json");
+  // xhr.send(JSON.stringify(data));
+
+  // application/x-www-form-urlencoded 类型（提交表单时使用的内容类型）
+  // 设置 Content-Type 为 application/x-www-form-urlencoded 时，-> express.urlencoded({ extend: true })
+  xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+  xhr.send("username=daixusheng&age=10");
+  
+  // application/javascript 类型（没跑通）
+  // const data = {bookName: "人生复本", author: "布莱克·克劳奇"};
+  // xhr.setRequestHeader("Content-Type", "application/javascript");
+  // xhr.send(data);
+
+
+  // PUT 请求
+  // const xhr = new XMLHttpRequest();
+  // xhr.open();
+  // xhr.send();
+
+  // DELETE 请求
+  // const xhr = new XMLHttpRequest();
+  // xhr.open();
+  // xhr.send();
 });
